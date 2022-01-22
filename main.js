0e8c0e67114aa578bf02ab7f0b12762f8a6614c7
Carla Serrano <carlacarly8@gmail.com>
2022-01-21 21:26:41 -0300

    solo se que no se nada

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-  container.classList.add('right-panel-active');
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-  container.classList.remove('right-panel-active');
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
+
+const perfil = {
+  name: "Jonathan",
+  email: "jonathan@gmail.com",
+  password: "123456",
+};
+
+const postPerfil = () => {
+  fetch("https://tiendavirtualmern.herokuapp.com/api", {
+    method: "POST",
+    body: JSON.stringify(perfil),
+    headers: {
+      "Content-type": "application/json",
+    }
+      .then((response) => response.json())
+      .then((data) => console.log(data)),
+  });
+};
