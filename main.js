05e498af731c290b7d30ad0ee1e7cef535472303
Carla Serrano <carlacarly8@gmail.com>
2022-01-24 18:02:38 -0300

    terminado

--- main.js
+++ main.js
@@ -1,29 +1,94 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
+const slide = () => {
+  const signUpButton = document.getElementById("signUp");
+  const signInButton = document.getElementById("signIn");
+  const container = document.getElementById("container");
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
+  signUpButton.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+  });
 
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
-
-const perfil = {
-  name: "Jonathan",
-  email: "jonathan@gmail.com",
-  password: "123456",
-};
-
-const postPerfil = () => {
-  fetch("https://tiendavirtualmern.herokuapp.com/api", {
-    method: "POST",
-    body: JSON.stringify(perfil),
-    headers: {
-      "Content-type": "application/json",
-    }
-      .then((response) => response.json())
-      .then((data) => console.log(data)),
+  signInButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
   });
 };
+
+// INICIAR SECION------------------------------------->
+const loing = () => {
+  const form = document.getElementById("loing");
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault;
+    const email = document.getElementById("emailLogin").value;
+    const password = document.getElementById("passwordLogin").value;
+    console.log(email, password);
+
+    try {
+      const res = await fetch(
+        "https://tiendavirtualmern.herokuapp.com/api/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+      if (res.status === 401) {
+        alert("Mail o contraceña incorrecta");
+      } else {
+        alert("Bienvenido");
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
+  });
+};
+
+// REGISTER ------------------------------------------>
+const register = () => {
+  const form = document.getElementById("register");
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const name = document.getElementById("nameRegister").value;
+    const email = document.getElementById("emailRegister").value;
+    const password = document.getElementById("passwordRegister").value;
+    console.log(email, password);
+
+    try {
+      const res = await fetch(
+        "https://tiendavirtualmern.herokuapp.com/api/users",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            password,
+          }),
+        }
+      );
+      if (res.status === 400) {
+        alert("El usuario ya existe");
+      } else if (res.status === 500) {
+        alert("Datos incompletos");
+      } else {
+        alert("Cuenta creada");
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
+  });
+};
+
+function main() {
+  slide();
+  loing();
+  register();
+}
+main();
